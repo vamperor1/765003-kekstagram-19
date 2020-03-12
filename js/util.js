791c7b7eb8ed03e99afef8c1c64e06511df23323
vamperor1 <vamperor@mail.ru>
2020-03-12 20:26:06 +0300

    Задание 4.1

--- js/util.js
+++ js/util.js
@@ -3,7 +3,7 @@
   window.util = {
     ESC_KEY: 'Escape',
     ENTER_KEY: 'Enter',
-    BODY: document.querySelector('body'),
+    // BODY: document.querySelector('body'),
     getRandomInRange: function (min, max) {
       return Math.floor(Math.random() * (max - min + 1)) + min;
     },
